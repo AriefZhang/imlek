a58e823df26449b7266719cb1f91464d02d3a7fc
Arief Nugroho <arief@nftseedlab.com>
2026-02-14 08:49:19 +0700

    update/script: move seeder data source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,25 +1,25 @@
-import { join } from 'path';
 import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 import type { DataSourceOptions } from 'typeorm';
 import type { SeederOptions } from 'typeorm-extension';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 config();
 
 const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'postgres',
-  // entities: [
-  //   join(__dirname, '../../modules', '**', '/entities', '*.entity.{ts,js}'),
-  // ],
   entities: ['dist/modules/**/entities/*.entity.ts'],
   synchronize: process.env.NODE_ENV === 'development',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_DB_NAME,
-  // url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
   seeds: ['src/database/seeds/*.seeder.{js,ts}'],
+  migrations: ['src/database/migrations/*.migration.{js,ts}'],
+  namingStrategy: new SnakeNamingStrategy(),
+  logging: ['error', 'schema'],
+  extra: {
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 30000,
+    max: 50,
+  },
 };
 
 export const dataSource = new DataSource(dataSourceOptions);
